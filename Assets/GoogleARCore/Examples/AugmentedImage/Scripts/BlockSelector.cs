e79af0c11d01c67ecaea4b46c6925ea2a73df49d
Oliver <oliek365@student.liu.se>
2020-04-16 13:30:16 +0200

    A good start

---
+++ Assets/GoogleARCore/Examples/AugmentedImage/Scripts/BlockSelector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSelector : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        _CheckTap();
+    }
+
+    private void _CheckTap()
+    {
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(raycast, out raycastHit))
+            {
+                GameObject hitObject = raycastHit.collider.gameObject;
+                hitObject.GetComponent<BlockBehavior>().OnSelect();
+            }
+        }
+    }
+}
